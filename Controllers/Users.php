c17839a87ccc29c207b3d01562d444d198f7e5ea
tyty21ppbb <panulde.artlaurenze21@gmail.com>
2026-09-21 23:43:00 +0800

    Refactor user retrieval to use UserModel

--- Controllers/Users.php
+++ Controllers/Users.php
@@ -2,37 +2,17 @@
 
 namespace App\Controllers;
 
+use App\Models\UserModel;
+
 class Users extends BaseController
 {
     public function index(): string
     {
-        $users = [
-            [
-                'username'  => 'admin',
-                'full_name' => 'Maloi Ricaldre',
-                'role'      => 'Administrator',
-            ],
-            [
-                'username'  => 'cashier01',
-                'full_name' => 'JL Torreliza',
-                'role'      => 'Cashier',
-            ],
-            [
-                'username'  => 'cashier02',
-                'full_name' => 'Akira Morishita',
-                'role'      => 'Cashier',
-            ],
-            [
-                'username'  => 'manager01',
-                'full_name' => 'Joy Barcoma',
-                'role'      => 'Manager',
-            ],
-            [
-                'username'  => 'stock01',
-                'full_name' => 'Baus Rufo',
-                'role'      => 'Inventory Clerk',
-            ],
-        ];
+        $userModel = new UserModel();
+
+        $users = $userModel
+            ->orderBy('id', 'ASC')
+            ->findAll();
 
         return view('users/index', [
             'title' => 'User Accounts',
